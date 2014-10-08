6b5e0daab688fac9befd91ce752366c99a01e32d
Nastasia <nastasia.mirani@gmail.com>
2014-10-08 19:08:42 +0530

    phpsdk

--- createevent.php
+++ createevent.php
@@ -1,8 +1,8 @@
 <?php
 
-$app_id = "APP_ID";
-$app_secret = "APP_SECRET";
-$my_url = "REDIRECT_URL";
+$app_id = "1486180144974468";
+$app_secret = "528b621709faf5bf2277b5272a1572e6";
+$my_url = "http://murmuring-plains-1063.herokuapp.com/"; //url
  
 $code = $_REQUEST["code"];
  
@@ -17,6 +17,7 @@ $token_url = "https://graph.facebook.com/oauth/access_token?client_id="
 . $app_id . "&redirect_uri=" . urlencode($my_url)
 . "&client_secret=" . $app_secret
 . "&code=" . $code;
+
 $access_token = file_get_contents($token_url);
  
 $event_url = "https://graph.facebook.com/me/events?" . $access_token;
@@ -39,7 +40,6 @@ input[type=text],textarea {width: 210px;}
     <p><label for="location">Location</label><input type="text" name="location" value="" /></p>
     <p><label for="">Start Time</label><input type="text" name="start_time" value="<?php echo date('Y-m-d H:i:s'); ?>" /></p>
     <p><label for="end_time">End Time</label><input type="text" name="end_time" value="<?php echo date('Y-m-d H:i:s', mktime(0, 0, 0, date("m")  , date("d")+1, date("Y"))); ?>" /></p>
-    <p><label for="picture">Event Picture</label><input type="file" name="picture" /></p>
     <p>
         <label for="privacy_type">Privacy</label>
         <input type="radio" name="privacy_type" value="OPEN" checked='checked'/>Open&nbsp;&nbsp;&nbsp;
