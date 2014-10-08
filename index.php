4a1909be6e48af90b373c3c62a367dc351f3e104
nmirani <nastasia.mirani@gmail.com>
2014-10-08 19:16:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
-
-$app_id = "1486180144974468";
-$app_secret = "528b621709faf5bf2277b5272a1572e6";
-<<<<<<< HEAD:createevent.php
-$my_url = "http://murmuring-plains-1063.herokuapp.com/"; //url
-=======
-$my_url = "http://murmuring-plains-1063.herokuapp.com/";
->>>>>>> 4eec946a87a0c0169eae71bbf25e09017436ec67:index.php
+session_start();
  
+require_once 'facebook-php-sdk/autoload.php';
+use Facebook\FacebookSession;
+use Facebook\FacebookRequest;
+use Facebook\GraphUser;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookRedirectLoginHelper;
+ 
+$app_id = '1486180144974468';
+$app_secret = '528b621709faf5bf2277b5272a1572e6';
+$redirect_login_url = 'http://murmuring-plains-1063.herokuapp.com/';
+
+FacebookSession::setDefaultApplication( $app_id, $app_secret );
+
 $code = $_REQUEST["code"];
  
 if(empty($code)) {
@@ -21,7 +27,6 @@ $token_url = "https://graph.facebook.com/oauth/access_token?client_id="
 . $app_id . "&redirect_uri=" . urlencode($my_url)
 . "&client_secret=" . $app_secret
 . "&code=" . $code;
-
 $access_token = file_get_contents($token_url);
  
 $event_url = "https://graph.facebook.com/me/events?" . $access_token;
@@ -44,6 +49,7 @@ input[type=text],textarea {width: 210px;}
     <p><label for="location">Location</label><input type="text" name="location" value="" /></p>
     <p><label for="">Start Time</label><input type="text" name="start_time" value="<?php echo date('Y-m-d H:i:s'); ?>" /></p>
     <p><label for="end_time">End Time</label><input type="text" name="end_time" value="<?php echo date('Y-m-d H:i:s', mktime(0, 0, 0, date("m")  , date("d")+1, date("Y"))); ?>" /></p>
+    <p><label for="picture">Event Picture</label><input type="file" name="picture" /></p>
     <p>
         <label for="privacy_type">Privacy</label>
         <input type="radio" name="privacy_type" value="OPEN" checked='checked'/>Open&nbsp;&nbsp;&nbsp;
@@ -54,3 +60,4 @@ input[type=text],textarea {width: 210px;}
 </form>
 </body>
 </html>
+
