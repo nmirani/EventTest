be6cef5dc3a62f64c21aa0cf386243ace821c29b
nmirani <nastasia.mirani@gmail.com>
2014-10-10 17:03:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,31 @@ $facebook = new Facebook(array(
 $access_token =  $facebook->getAccessToken();
 $facebook->setAccessToken($access_token);
 
-//List tester
+// Get User ID
+$user = $facebook->getUser();
+
+// We may or may not have this data based on whether the user is logged in.
+//
+// If we have a $user id here, it means we know the user is logged into
+// Facebook, but we don't know if the access token is valid. An access
+// token is invalid if the user logged out of Facebook.
+
+if ($user) {
+  try {
+    // Proceed knowing you have a logged in user who's authenticated.
+    $user_profile = $facebook->api('/me');
+  } catch (FacebookApiException $e) {
+    error_log($e);
+    $user = null;
+  }
+}
+
+// Login or logout url will be needed depending on current user state.
+if ($user) {
+  $logoutUrl = $facebook->getLogoutUrl();
+} else {
+  $loginUrl = $facebook->getLoginUrl();
+}
 
 $request = new FacebookRequest(
   $session,
