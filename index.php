ec8b8193951495153e53c71d5d2c5784e630d529
nmirani <nastasia.mirani@gmail.com>
2014-10-10 15:35:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,52 +1,33 @@
 <?php
 
+include_once "facebook-php-sdk/src/facebook.php";
+
+
 $app_id = "1486180144974468";
 $app_secret = "528b621709faf5bf2277b5272a1572e6";
 $my_url = "http://murmuring-plains-1063.herokuapp.com/";
- 
-$code = $_REQUEST["code"];
- 
-if(empty($code)) {
-    $auth_url = "http://www.facebook.com/dialog/oauth?client_id="
-    . $app_id . "&redirect_uri=" . urlencode($my_url)
-    . "&scope=create_event";
-    echo("<script>top.location.href='" . $auth_url . "'</script>");
-}
- 
-$token_url = "https://graph.facebook.com/oauth/access_token?client_id="
-. $app_id . "&redirect_uri=" . urlencode($my_url)
-. "&client_secret=" . $app_secret
-. "&code=" . $code;
-$access_token = file_get_contents($token_url);
- 
-$event_url = "https://graph.facebook.com/me/events?" . $access_token;
-?>
 
+$facebook = new Facebook(array(
+						'appId' => $app_id,
+						'secret' => $app_secret,
+						'cookie' => true));
+						
+$access_token =  $facebook->getAccessToken();
+$facebook->setAccessToken($access_token);
 
-<!doctype html>
-<html>
-<head>
-<title>Create An Event</title>
-<style>
-label {float: left; width: 100px;}
-input[type=text],textarea {width: 210px;}
-</style>
-</head>
-<body>
-<form enctype="multipart/form-data" action="<?php echo $event_url; ?>" method="post">
-    <p><label for="name">Event Name</label><input type="text" name="name" value="" /></p>
-    <p><label for="description">Event Description</label><textarea name="description"></textarea></p>
-    <p><label for="location">Location</label><input type="text" name="location" value="" /></p>
-    <p><label for="">Start Time</label><input type="text" name="start_time" value="<?php echo date('Y-m-d H:i:s'); ?>" /></p>
-    <p><label for="end_time">End Time</label><input type="text" name="end_time" value="<?php echo date('Y-m-d H:i:s', mktime(0, 0, 0, date("m")  , date("d")+1, date("Y"))); ?>" /></p>
-    <p><label for="picture">Event Picture</label><input type="file" name="picture" /></p>
-    <p>
-        <label for="privacy_type">Privacy</label>
-        <input type="radio" name="privacy_type" value="OPEN" checked='checked'/>Open&nbsp;&nbsp;&nbsp;
-        <input type="radio" name="privacy_type" value="CLOSED" />Closed&nbsp;&nbsp;&nbsp;
-        <input type="radio" name="privacy_type" value="SECRET" />Secret&nbsp;&nbsp;&nbsp;
-    </p>
-    <p><input type="submit" value="Create Event" /></p>
-</form>
-</body>
-</html>
+//List tester
+
+$request = new FacebookRequest(
+  $session,
+  'GET',
+  '/{347776302055884}/attending'
+);
+$response = $request->execute();
+$graphObject = $response->getGraphObject();
+
+<h1> People attending event </h1>
+echo $request;
+/* handle the result */
+
+ 
+
