b0f16a3cc0749363f38642ab823199fd0ae3fa48
nmirani <nastasia.mirani@gmail.com>
2014-10-13 01:45:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,60 +13,20 @@ $facebook = new Facebook(array(
 				'secret' => $app_secret,
 				'cookie' => true));
 				
-				
-$session = Facebook\FacebookSession::newAppSession('1486180144974468','528b621709faf5bf2277b5272a1572e6');
-print_r($session);
-
-session_start();
-
-
-//getting access token
 $access_token =  $facebook->getAccessToken();
 $facebook->setAccessToken($access_token);
-print_r($facebook);
 
-//setting login and including the required permissions
-$helper = new FacebookRedirectLoginHelper( 'http://agile-badlands-9486.herokuapp.com/' );
-$helper->getLoginUrl(array('scope' => 'user_events'));
+$session = new FacebookSession('$access_token');
 
-// Get User ID
-$user = $facebook->getUser();
-$facebook->api($url, 'GET');
-
-// see if a existing session exists
-if ( isset( $_SESSION ) && isset( $_SESSION['fb_token'] ) ) {
-  // create new session from saved access_token
-  $session = new FacebookSession( $_SESSION['fb_token'] );
-  
-  // validate the access_token to make sure it's still valid
-  try {
-    if ( !$session->validate() ) {
-      $session = null;
-      print_r($session);
-
-    }
-  } catch ( Exception $e ) {
-    // catch any exceptions
-    $session = null;
-    print_r($session);
-  }
+try {
+  $me = (new FacebookRequest(
+    $session, 'GET', '/me'
+  ))->execute()->getGraphObject();
+  echo $me->getName();
+} catch (FacebookRequestException $e) {
+  // The Graph API returned an error
+  print_r $e;
+} catch (\Exception $e) {
+  // Some other error occurred
+  print_r $e;
 }
-
-// see if we have a session
-if ( isset( $session ) ) {
-  
-  // save the session
-  $_SESSION['fb_token'] = $session->getToken();
-  // create a session using saved token or the new one we generated at login
-  $session = new FacebookSession( $session->getToken() );
-  
-  $request = new FacebookRequest( $session, 'GET', '/{347776302055884}/attending' );
-  $response = $request->execute();
-  // get response
-  $graphObject = $response->getGraphObject()->asArray();
-
-  echo '<pre>' . print_r( $graphObject, 1 ) . '</pre>';
-  
-  error_reporting(E_ALL);
-	ini_set('display_errors', 1); }
-	?>
