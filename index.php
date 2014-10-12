9e5a200ae7757c69c3ade9b599233fa83dc0b5d7
nmirani <nastasia.mirani@gmail.com>
2014-10-12 20:56:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,47 +11,69 @@ $facebook = new Facebook(array(
 						'appId' => $app_id,
 						'secret' => $app_secret,
 						'cookie' => true));
-						
-$access_token =  $facebook->getAccessToken();
-$facebook->setAccessToken($access_token);
-
-// Get User ID
-$user = $facebook->getUser();
-
-// We may or may not have this data based on whether the user is logged in.
-//
-// If we have a $user id here, it means we know the user is logged into
-// Facebook, but we don't know if the access token is valid. An access
-// token is invalid if the user logged out of Facebook.
-
-if ($user) {
-  try {
-    // Proceed knowing you have a logged in user who's authenticated.
-    $user_profile = $facebook->api('/me');
-  } catch (FacebookApiException $e) {
-    error_log($e);
-    $user = null;
-  }
-}
-
-// Login or logout url will be needed depending on current user state.
-if ($user) {
-  $logoutUrl = $facebook->getLogoutUrl();
-} else {
-  $loginUrl = $facebook->getLoginUrl();
-}
-
-$request = new FacebookRequest(
-  $session,
-  'GET',
-  '/{347776302055884}/attending'
-);
-$response = $request->execute();
-$graphObject = $response->getGraphObject();
-
-<h1> People attending event </h1>
-echo $request;
-/* handle the result */
-
+// start session
+session_start();
  
-
+// init app with app id and secret
+FacebookSession::setDefaultApplication( '1486180144974468','528b621709faf5bf2277b5272a1572e6' );
+ 
+// login helper with redirect_uri
+$helper = new FacebookRedirectLoginHelper( 'http://murmuring-plains-1063.herokuapp.com/' );
+ 
+// see if a existing session exists
+if ( isset( $_SESSION ) && isset( $_SESSION['fb_token'] ) ) {
+  // create new session from saved access_token
+  $session = new FacebookSession( $_SESSION['fb_token'] );
+  
+  // validate the access_token to make sure it's still valid
+  try {
+    if ( !$session->validate() ) {
+      $session = null;
+    }
+  } catch ( Exception $e ) {
+    // catch any exceptions
+    $session = null;
+  }
+}  
+ 
+if ( !isset( $session ) || $session === null ) {
+  // no session exists
+  
+  try {
+    $session = $helper->getSessionFromRedirect();
+  } catch( FacebookRequestException $ex ) {
+    // When Facebook returns an error
+    // handle this better in production code
+    print_r( $ex );
+  } catch( Exception $ex ) {
+    // When validation fails or other local issues
+    // handle this better in production code
+    print_r( $ex );
+  }
+  
+}
+ 
+// see if we have a session
+if ( isset( $session ) ) {
+  
+  // save the session
+  $_SESSION['fb_token'] = $session->getToken();
+  // create a session using saved token or the new one we generated at login
+  $session = new FacebookSession( $session->getToken() );
+  
+  // graph api request for user data
+  $request = new FacebookRequest( $session, 'GET', '/347776302055884/attending' );
+  $response = $request->execute();
+  // get response
+  $graphObject = $response->getGraphObject()->asArray();
+  
+  // print profile data
+  echo '<pre>' . print_r( $graphObject, 1 ) . '</pre>';
+  
+  // print logout url using session and redirect_uri (logout.php page should destroy the session)
+  echo '<a href="' . $helper->getLogoutUrl( $session, 'http://yourwebsite.com/app/logout.php' ) . '">Logout</a>';
+  
+} else {
+  // show login url
+  echo '<a href="' . $helper->getLoginUrl( array( 'email', 'user_friends' ) ) . '">Login</a>';
+}
