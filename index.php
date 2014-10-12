55e3f4c534f00600a90efd33e8bf8a9d765a3d5f
nmirani <nastasia.mirani@gmail.com>
2014-10-12 23:01:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,20 +8,20 @@
 */
 
 /* INCLUSION OF LIBRARY FILEs*/
-	require_once( 'lib/Facebook/FacebookSession.php');
-	require_once( 'lib/Facebook/FacebookRequest.php' );
-	require_once( 'lib/Facebook/FacebookResponse.php' );
-	require_once( 'lib/Facebook/FacebookSDKException.php' );
-	require_once( 'lib/Facebook/FacebookRequestException.php' );
-	require_once( 'lib/Facebook/FacebookRedirectLoginHelper.php');
-	require_once( 'lib/Facebook/FacebookAuthorizationException.php' );
-	require_once( 'lib/Facebook/GraphObject.php' );
-	require_once( 'lib/Facebook/GraphUser.php' );
-	require_once( 'lib/Facebook/GraphSessionInfo.php' );
-	require_once( 'lib/Facebook/Entities/AccessToken.php');
-	require_once( 'lib/Facebook/HttpClients/FacebookCurl.php' );
-	require_once( 'lib/Facebook/HttpClients/FacebookHttpable.php');
-	require_once( 'lib/Facebook/HttpClients/FacebookCurlHttpClient.php');
+	require_once( 'facebook-php-sdk/src/Facebook/FacebookSession.php');
+	require_once( 'facebook-php-sdk/src/Facebook/FacebookRequest.php' );
+	require_once( 'facebook-php-sdk/src/Facebook/FacebookResponse.php' );
+	require_once( 'facebook-php-sdk/src/Facebook/FacebookSDKException.php' );
+	require_once( 'facebook-php-sdk/src/Facebook/FacebookRequestException.php' );
+	require_once( 'facebook-php-sdk/src/Facebook/FacebookRedirectLoginHelper.php');
+	require_once( 'facebook-php-sdk/src/Facebook/FacebookAuthorizationException.php' );
+	require_once( 'facebook-php-sdk/src/Facebook/GraphObject.php' );
+	require_once( 'facebook-php-sdk/src/Facebook/GraphUser.php' );
+	require_once( 'facebook-php-sdk/src/Facebook/GraphSessionInfo.php' );
+	require_once( 'facebook-php-sdk/src/Facebook/Entities/AccessToken.php');
+	require_once( 'facebook-php-sdk/src/Facebook/HttpClients/FacebookCurl.php' );
+	require_once( 'facebook-php-sdk/src/Facebook/HttpClients/FacebookHttpable.php');
+	require_once( 'facebook-php-sdk/src/Facebook/HttpClients/FacebookCurlHttpClient.php');
 
 /* USE NAMESPACES */
 	
