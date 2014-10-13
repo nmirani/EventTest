77fc079f5ba6f78e006a727b214675cc7ab31875
nmirani <nastasia.mirani@gmail.com>
2014-10-13 17:39:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,67 +1,57 @@
 <?php
 
 session_start();
-require_once 'autoload.php';
-use Facebook\FacebookSession;
-use Facebook\FacebookRequest;
-use Facebook\GraphUser;
-use Facebook\FacebookRequestException;
-use Facebook\FacebookRedirectLoginHelper;
 
 include_once "facebook-php-sdk/src/facebook.php";
-
-
-$app_id = '1486180144974468';
-$app_secret = '528b621709faf5bf2277b5272a1572e6';
-$url = 'http://agile-badlands-9486.herokuapp.com/';
-
-
-
-$facebook = new Facebook(array(
-				'appId' => $app_id,
-				'secret' => $app_secret,
-				'cookie' => true));
-				
-
-
-$session = Facebook\FacebookSession::setDefaultApplication('1486180144974468','528b621709faf5bf2277b5272a1572e6');
-$access_token =  $facebook->getAccessToken();
-$facebook->setAccessToken($access_token);
-print_r($session);
-
-$session = new FacebookSession('$access_token');
-
-//setting login and including the required permissions
+ 
+require_once( 'facebook-php-sdk/src/Facebook/FacebookSession.php' );
+require_once( 'facebook-php-sdk/src/Facebook/FacebookRedirectLoginHelper.php' );
+require_once( 'facebook-php-sdk/src/Facebook/FacebookRequest.php' );
+require_once( 'facebook-php-sdk/src/Facebook/FacebookResponse.php' );
+require_once( 'facebook-php-sdk/src/Facebook/FacebookSDKException.php' );
+require_once( 'facebook-php-sdk/src/Facebook/FacebookRequestException.php' );
+require_once( 'facebook-php-sdk/src/Facebook/FacebookAuthorizationException.php' );
+require_once( 'facebook-php-sdk/src/Facebook/GraphObject.php' );
+require_once 'autoload.php';
+ 
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookResponse;
+use Facebook\FacebookSDKException;
+use Facebook\FacebookRequestException;
+use Facebook\FacebookAuthorizationException;
+use Facebook\GraphObject;
+ 
+// init app with app id (APPID) and secret (SECRET)
+FacebookSession::setDefaultApplication('1486180144974468','528b621709faf5bf2277b5272a1572e6');
+ 
+// login helper with redirect_uri
 $helper = new FacebookRedirectLoginHelper( 'http://agile-badlands-9486.herokuapp.com/' );
-$helper->getLoginUrl(array('scope' => 'user_events'));
-
-
+ 
 try {
-	if(isset($session)){
-	
-  $request = new FacebookRequest(
-  $session,
-  'GET',
-  '/{347776302055884}/attending'
-);
-$response = $request->execute();
-$graphObject = $response->getGraphObject();
- print_r $graphObject;
- echo "List of attendees" $graphObject;
- } else {
+  $session = $helper->getSessionFromRedirect();
+} catch( FacebookRequestException $ex ) {
+  // When Facebook returns an error
+  echo print_r ($ex "Exception");
+} catch( Exception $ex ) {
+  // When validation fails or other local issues
+  echo print_r ($ex "Exception");
+}
+ 
+// see if we have a session
+if ( isset( $session ) ) {
+  // graph api request for user data
+  $request = new FacebookRequest( $session, 'GET', '/me' );
+  $response = $request->execute();
+  // get response
+  $graphObject = $response->getGraphObject();
+   
+  // print data
+  echo  print_r( $graphObject, 1 );
+} else {
   // show login url
   echo '<a href="' . $helper->getLoginUrl() . '">Login</a>';
 }
-	
-} catch (FacebookRequestException $e) {
-	print_r($e);
-  // The Graph API returned an error
-  print_r $e;
-} catch (\Exception $e) {
-  // Some other error occurred
-  print_r $e;
-   error_reporting(E_ALL);
-	ini_set('display_errors', 1);
-}
- 
+
 ?>
